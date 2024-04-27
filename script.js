0639606223c2921ade7b798f02e4913c5e674def
Raj Kumar Singh <rajkumarsingh9164@gmail.com>
2024-04-27 10:29:44 +0530

    Menu Style updated

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display= 'none'
+}
